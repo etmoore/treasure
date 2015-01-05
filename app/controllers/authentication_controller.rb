2cb3f1837fb872e812d2f8dec706ce222c10eee2
Evan Moore <etmoore@gmail.com>
2015-01-04 18:54:25 -0700

    users can sign in and out

---
+++ app/controllers/authentication_controller.rb
@@ -0,0 +1,20 @@
+class AuthenticationController < ApplicationController
+  def new
+  end
+
+  def create
+    user = User.find_by_email(params[:authentication][:email])
+    if user && user.authenticate(params[:authentication][:password])
+      session[:user_id] = user.id
+      redirect_to root_path
+    else
+      @sign_in_error = "Username/password combination is invalid."
+      render :new
+    end
+  end
+
+  def destroy
+    session.clear
+    redirect_to root_path
+  end
+end
