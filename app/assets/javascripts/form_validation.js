ea3d717269a32bfe79d54921ece800b0e3b53002
Evan Moore <etmoore@gmail.com>
2015-01-13 14:29:34 -0700

    error message hides when user focuses on textarea

--- app/assets/javascripts/form_validation.js
+++ app/assets/javascripts/form_validation.js
@@ -1,8 +1,23 @@
-$(document).ready(function() {
-  $('textarea').on('blur', function() {
+$(function(){
+
+  var Validator = {
+    show_error: function() {
+      $('.error').show();
+    },
+    hide_error: function() {
+      $('.error').hide();
+    },
+  };
+
+  // hide the error message when the user clicks in the textarea
+  $('textarea').on('focus', Validator.hide_error);
+
+  // show error if the message doesn't start with a handle
+  $('textarea').on('blur', function(){
     var tweetMessage = $('#tweet_message').val();
-    if(tweetMessage[0] !== '@') {
-      console.log('missing handle');
+    if (tweetMessage[0] !== '@') {
+      Validator.show_error();
     }
   });
+
 });
--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -8,5 +8,6 @@ Rails.application.config.assets.version = '1.0'
 
 # Precompile additional assets.
 # application.js, application.css, and all non-JS/CSS in app/assets folder are already added.
-# Rails.application.config.assets.precompile += %w( search.js )
-Rails.application.config.assets.precompile += %w( form_validation.js )
+js_files = %w(form_validation.js)
+
+Rails.application.config.assets.precompile += js_files
