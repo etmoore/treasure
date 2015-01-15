66274925503e7af3e208691593bc2892eff85b83
Evan Moore <etmoore@gmail.com>
2015-01-14 20:33:47 -0700

    prepared user and authentication models for twitter auth

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,5 @@
 class User < ActiveRecord::Base
-  validates :email, presence: true, uniqueness: true
-  validates :first_name, :last_name, presence: true
-  has_secure_password
+  has_many :authentications
 
   def tweet(tweet)
     client = Twitter::REST::Client.new do |config|
@@ -10,19 +8,7 @@ class User < ActiveRecord::Base
       config.access_token        = Rails.application.secrets.access_token
       config.access_token_secret = Rails.application.secrets.access_token_secret
     end
-    if tweet[0] == '@'
-      words = tweet.split(' ')
-      id_len = words[0].length
-      name = tweet[1, id_len]
-      puts client.user(name)
-    else
-      raise MissingName
-    end
     client.update(tweet)
-  end
-
-  def full_name
-    "#{self.first_name} #{self.last_name}"
   end
 
   def admin?
