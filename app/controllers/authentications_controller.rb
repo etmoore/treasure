95802d37939d0cb9d5fe6f06827d8abccad9c40d
Evan Moore <etmoore@gmail.com>
2015-01-16 12:15:02 -0700

    users can now log in with twitter

--- app/controllers/authentications_controller.rb
+++ app/controllers/authentications_controller.rb
@@ -1,8 +1,11 @@
 class AuthenticationsController < ApplicationController
 
   def create
-    omniauth = env['omniauth.auth']
-    render json: omniauth
+    authentication = Authentication.from_omniauth(env['omniauth.auth'])
+    session[:user_id] = authentication.user_id
+    redirect_to root_path
+    # render json: omniauth
+
   end
 
   def destroy
--- app/models/authentication.rb
+++ app/models/authentication.rb
@@ -1,3 +1,19 @@
 class Authentication < ActiveRecord::Base
   belongs_to :user
+
+  def self.from_omniauth(auth)
+    where(provider: auth['provider'], uid: auth['uid']).first || create_from_omniauth(auth)
+  end
+
+  def self.create_from_omniauth(auth)
+    user = User.create(
+      twitter: auth['info']['nickname'],
+    )
+    user.authentications.create(
+      provider: auth["provider"],
+      uid: auth["uid"],
+      token: auth["credentials"]["token"],
+      secret: auth["credentials"]["secret"],
+    )
+  end
 end
